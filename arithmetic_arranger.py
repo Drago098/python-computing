f92f85a4d6552a45fdfa5303589f4d1fa0418d2d
Ayush Pandey <pandeyayush590@gmail.com>
2021-05-19 23:16:15 +0530

    Update arithmetic_arranger.py

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,64 +1,131 @@
-def arithmetic_arranger(problems, answer=False):
-    # Check the number of problems
+def arithmetic_arranger(problems, display=False):
+    
+    operators = []
+    longest_operands = []
+    len_diffs = []
+
+    num_spaces = []
+
+    print_list = []
+    print_list_2 = []
+    print_list_3 = []
+
     if len(problems) > 5:
         return "Error: Too many problems."
     
-    first_operand = []
-    second_operand = []
-    operator = []
+    for problem in range(len(problems)):
+        if "+" in problems[problem]:
+            problems[problem] = problems[problem].split(" + ")
+            operators.append("+")
+            
+        elif "-" in problems[problem]:
+            problems[problem] = problems[problem].split(" - ")
+            operators.append("-")
 
-    for problem in problems:
-        pieces = problem.split()
-        first_operand.append(pieces[0])
-        operator.append(pieces[1])
-        second_operand.append(pieces[2])
+        else:
+            return "Error: Operator must be '+' or '-'."
 
-    # Check for * or /
-    if "*" in operator or "/" in operator:
-        return "Error: Operator must be '+' or '-'."
-
-    # Check the digits
-    for i in range(len(first_operand)):
-        if not (first_operand[i].isdigit() and second_operand[i].isdigit()):
+    for problem in range(len(problems)):
+        if problems[problem][0].isdigit() == False:
+            return "Error: Numbers must only contain digits."
+        elif problems[problem][1].isdigit() == False:
             return "Error: Numbers must only contain digits."
 
-    # Check the length
-    for i in range(len(first_operand)):
-        if len(first_operand[i]) > 4 or len(second_operand[i]) > 4:
+    for problem in range(len(problems)):
+        if len(problems[problem][0]) > 4:
+            return "Error: Numbers cannot be more than four digits."
+        elif len(problems[problem][1]) > 4:
             return "Error: Numbers cannot be more than four digits."
 
-    first_line = []
-    second_line = []
-    third_line = []
-    fourth_line = []
+    for problem in range(len(problems)):
+        if len(problems[problem][0]) < len(problems[problem][1]):
+            longest_operands.append("1")
 
-    for i in range(len(first_operand)):
-        if len(first_operand[i]) > len(second_operand[i]):
-            first_line.append(" "*2 + first_operand[i])
+            len_diffs.append(1 + (len(problems[problem][1])-len(problems[problem][0])))
         else:
-            first_line.append(" "*(len(second_operand[i]) - len(first_operand[i]) + 2) + first_operand[i])
+            longest_operands.append("-1")
 
-    for i in range(len(second_operand)):
-        if len(second_operand[i]) > len(first_operand[i]):
-            second_line.append(operator[i] + " " + second_operand[i])
+            len_diffs.append(-1 - (len(problems[problem][0])-len(problems[problem][1])))
+
+    #---------------------------------
+
+    for lens in range(len(len_diffs)):
+        if len_diffs[lens] > 0:
+            for num_times_print in range(len_diffs[lens]):
+                print_list.append(" ")
+            print_list.append(" ")
+            print_list.append(problems[lens][0])
+
+            if lens != (len(problems) - 1):
+                print_list.append("    ")
         else:
-            second_line.append(operator[i] + " "*(len(first_operand[i]) - len(second_operand[i]) + 1) + second_operand[i])
+            print_list.append("  ")
+            print_list.append(problems[lens][0])
 
-    for i in range(len(first_operand)):
-        third_line.append("-"*(max(len(first_operand[i]), len(second_operand[i])) + 2))
+            if lens != (len(problems) - 1):
+                print_list.append("    ")
 
-    if answer:
-        for i in range(len(first_operand)):
-            if operator[i] == "+":
-                ans = str(int(first_operand[i]) + int(second_operand[i]))
+    for lens in range(len(len_diffs)):
+        print_list_2.append(operators[lens])
+        if len_diffs[lens] < 0:
+            for spaces in range(abs(len_diffs[lens])):
+                print_list_2.append(" ")
+            print_list_2.append(problems[lens][1])
+
+            if lens != (len(problems) - 1):
+                print_list_2.append("    ")
+        else:
+            print_list_2.append(" ")
+            print_list_2.append(problems[lens][1])
+
+            if lens != (len(problems) - 1):
+                print_list_2.append("    ")
+
+    for operands in range(len(longest_operands)):
+        if longest_operands[operands] == "-1":
+            for number_of_dashes in range(len(problems[operands][0])+2):
+                print_list_3.append("-")
+        else:
+            for number_of_dashes in range(len(problems[operands][1])+2):
+                print_list_3.append("-")
+                
+
+        if operands != (len(problems) - 1):
+            print_list_3.append("    ")
+
+    return_statement = ''.join(print_list) + "\n" + ''.join(print_list_2) + "\n" + ''.join(print_list_3)
+    
+    if display == True:
+        solutions = []
+        print_list_4 = []
+        for problem in range(len(problems)):
+            if operators[problem] == "+":
+                solutions.append(int(problems[problem][0]) + int(problems[problem][1]))
             else:
-                ans = str(int(first_operand[i]) - int(second_operand[i]))
+                solutions.append(int(problems[problem][0]) - int(problems[problem][1]))
 
-            if len(ans) > max(len(first_operand[i]), len(second_operand[i])):
-                fourth_line.append(" " + ans)
+        for problem in range(len(problems)):
+            if len(problems[problem][0]) > len(problems[problem][1]):
+                num_spaces.append(len(str(problems[problem][0])) - len(str(solutions[problem])))
             else:
-                fourth_line.append(" "*(max(len(first_operand[i]), len(second_operand[i])) - len(ans) + 2) + ans)
-        arranged_problems = "    ".join(first_line) + "\n" + "    ".join(second_line) + "\n" + "    ".join(third_line) + "\n" + "    ".join(fourth_line)
+                num_spaces.append(len(str(problems[problem][1])) - len(str(solutions[problem])))
+
+        for spaces in range(len(num_spaces)):
+            for i in range(2 + int(num_spaces[spaces])):
+                print_list_4.append(" ")
+                
+            print_list_4.append(solutions[spaces])
+
+            if spaces != (len(problems) - 1):
+                print_list_4.append("    ")
+
+        for i in range(len(print_list_4)):
+            if isinstance(print_list_4[i], int):
+                print_list_4[i] = str(print_list_4[i])
+
+        return_statement = ''.join(print_list) + "\n" + ''.join(print_list_2) + "\n" + ''.join(print_list_3) + "\n" + ''.join(print_list_4)
+            
+        return return_statement
+
     else:
-        arranged_problems = "    ".join(first_line) + "\n" + "    ".join(second_line) + "\n" + "    ".join(third_line)
-    return arranged_problems
+        return return_statement
